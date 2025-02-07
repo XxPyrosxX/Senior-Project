aac30a0a1ab5b6e5716d16feac26a0931a9561ef
DivyanshiSaini <sainid@ufl.edu>
2025-02-07 12:49:51 -0500

    Navigaton Tabs

--- KitchenSync/app/_layout.tsx
+++ KitchenSync/app/_layout.tsx
@@ -1,5 +1,16 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  //return <Stack screenOptions={{ headerShown: false }} />;
+ return(
+ <Tabs>
+     <Stack.Screen name="pages/HomePage" options={{ title: 'Login' }} />
+     <Tabs.Screen name="pages/Pantry" options={{ title: "Pantry" }} />
+
+      //hide a page 
+     <Tabs.Screen name="index" options={{ href: null }} />
+    
+  </Tabs>
+  );
+ 
 }
